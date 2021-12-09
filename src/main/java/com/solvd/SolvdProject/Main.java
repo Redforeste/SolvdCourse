0d44c5c30140d508c484a27ccb4bfbe9c4feb9c6
pavlo <pavlostasyk@gmail.com>
2021-12-09 20:13:41 +0200

    /

--- src/main/java/com/solvd/SolvdProject/Main.java
+++ src/main/java/com/solvd/SolvdProject/Main.java
@@ -1,7 +1,40 @@
 package com.solvd.SolvdProject;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Хочу залік...:(");
+
+    public static void bubbleSort(int[] array) {
+        boolean unsorted = true;
+        int temp;
+        while (unsorted) {
+            unsorted = false;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    unsorted = true;
+                }
+            }
+        }
     }
-}
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+
+            System.out.println("Enter array length: ");
+            int size = input.nextInt();
+            int array[] = new int[size];
+            System.out.println("Insert array elements:");
+
+            for (int i = 0; i < size; i++) {
+                array[i] = input.nextInt();
+            }
+
+
+            bubbleSort(array);
+            System.out.println(Arrays.toString(array));
+        }
+    }
+
