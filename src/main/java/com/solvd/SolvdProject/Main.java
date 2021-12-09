f8aaee63019068e6c6da965af3262875de2bccd7
pavlo <pavlostasyk@gmail.com>
2021-12-09 19:00:21 +0200

    /

--- src/main/java/com/solvd/SolvdProject/Main.java
+++ src/main/java/com/solvd/SolvdProject/Main.java
@@ -2,6 +2,6 @@ package com.solvd.SolvdProject;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Хочу залік..:(");
+        System.out.println("Хочу залік...:(");
     }
 }
